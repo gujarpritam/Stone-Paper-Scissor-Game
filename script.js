9d87d74aeec29b02b8534a0eb0d701964b9a2cec
Pritam <pritamgujar27@gmail.com>
2023-12-14 13:24:52 +0530

    Stone, Paper & Scissor Game

---
+++ script.js
@@ -0,0 +1,7 @@
+function rules() {
+    document.getElementById('game-rules').setAttribute("style", "display:flex");
+}
+
+function closeRules() {
+    document.getElementById('game-rules').setAttribute("style", "display:none");
+}
